6afd778f1f57fd54d5601ce1712e3b3be2b1b0b7
Cody Hmelar <ceh214@pitt.edu>
2025-02-24 11:38:34 -0500

    Update deploy.sh

--- timeline-app/deploy.sh
+++ timeline-app/deploy.sh
@@ -5,14 +5,15 @@ set -e
 
 env PUBLIC_URL=https://github.com/codyhmelar13/Language-Access-Database/tree/main npm run build
 
+
 cd build
 
 git init
 git add -A
 git commit -m 'deploy'
 
-deploying to https://{USERNAME}.github.io (see https://pages.github.com for instructions)
-git push -f git@github.com:{USERNAME}/{USERNAME}.github.io.git main
+deploying to https://{codyhmelar13.github.io (see https://pages.github.com for instructions)
+git push -f git@github.com:codyhmelar13/codyhmelar13.github.io.git main
 
-deploying to https://{USERNAME}.github.io/{REPO}
+deploying to https://codyhmelar13.github.io/language-access-database
 git push -f git@github.com:codyhmelar13/Language-Access-Database.git main:gh-pages
